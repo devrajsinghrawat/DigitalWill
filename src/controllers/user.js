1a2ecf3376cb19f07fbdd74a09cd36c8173fa622
12lucky21 <kumar.rajneesh86@gmail.com>
2022-09-05 11:27:05 +0530

    Add files via upload

---
+++ src/controllers/user.js
@@ -0,0 +1,136 @@
+const 	userModel 	= require('../model/user'),
+		helper      = require('../helpers/index');
+		
+let user = {};
+
+// User Register
+user.addUser = async function (req, res, next) {
+	if(req.body.UserPublicKey && req.body.UserPublicKey != ''){		
+		userModel.addUser(req.body).then(function (result) {
+			console.log('result : ', result);
+			if (result.status) {
+				if(result.status == true){
+					helper.successHandler(res, {
+						data: result.message
+					}, 200);
+				} else {
+					helper.errorHandler(res, {
+						status: false,
+						message: result.message
+					}, 500);
+				}
+			} else {
+				helper.errorHandler(res, {
+					status: false,
+					message: result.message
+				}, 500);
+			}         
+		});
+	} else {
+		helper.errorHandler(res, {
+			status: false,
+			message: "Post data empty !!"
+		}, 500);
+	}
+}
+
+user.updateUser = function (req, res, next) {
+        if (req.body.UserPublicKey) {
+            userModel.updateUser(req.body).then(function (result) {
+				console.log(result);
+                if (result) {
+					helper.successHandler(res, {
+						data: result.data
+					}, 200);
+                } else {
+                    helper.errorHandler(res, {
+						status: false,
+						message: result.message
+					}, 500);
+                }
+            });
+        } else {
+            helper.errorHandler(res, {
+				status: false,
+				message: "Something went wrong !!"
+			}, 500);
+        }
+}
+
+user.getUser = function (req, res, next) {
+	if (req.body) {
+		userModel.getUser(req.body).then(function (result) {
+			console.log(result);
+			if (result) {
+				helper.successHandler(res, {
+					data: result.data
+				}, 200);
+			} else {
+				helper.errorHandler(res, {
+					status: false,
+					message: result.message
+				}, 500);
+			}
+		});
+	} else {
+		helper.errorHandler(res, {
+			status: false,
+			message: "Something went wrong !!"
+		}, 500);
+	}
+}
+
+user.AddNominee = async function (req, res, next) {
+	if(req.body.UserPublicKey && req.body.UserPublicKey != ''){		
+		userModel.AddNominee(req.body).then(function (result) {
+			console.log('result : ', result);
+			if (result.status) {
+				if(result.status == true){
+					helper.successHandler(res, {
+						data: result.message
+					}, 200);
+				} else {
+					helper.errorHandler(res, {
+						status: false,
+						message: result.message
+					}, 500);
+				}
+			} else {
+				helper.errorHandler(res, {
+					status: false,
+					message: result.message
+				}, 500);
+			}         
+		});
+	} else {
+		helper.errorHandler(res, {
+			status: false,
+			message: "Post data empty !!"
+		}, 500);
+	}
+}
+
+user.UpdateNominee = function (req, res, next) {
+	if (req.body.UserPublicKey) {
+		userModel.UpdateNominee(req.body).then(function (result) {
+			console.log(result);
+			if (result) {
+				helper.successHandler(res, {
+					data: result.data
+				}, 200);
+			} else {
+				helper.errorHandler(res, {
+					status: false,
+					message: result.message
+				}, 500);
+			}
+		});
+	} else {
+		helper.errorHandler(res, {
+			status: false,
+			message: "Something went wrong !!"
+		}, 500);
+	}
+}
+
+module.exports = user;
