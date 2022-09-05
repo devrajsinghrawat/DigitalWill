f45c2c35a1c0e675f43da4280f7d852d4b9308f6
rajneesh1602 <101247330+rajneesh1602@users.noreply.github.com>
2022-09-05 11:29:53 +0530

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,6 +11,7 @@ module.exports = function() {
 
 	app.post('/AddNominee', userController.AddNominee);
 	app.post('/UpdateNominee', userController.UpdateNominee);
+	//app.post('/UpdateNominee', userController.UpdateNominee);
 
 }
 
