e7fe76b3799b2d296157517062a3f7815cf064c5
Devraj Singh <devraj.singh.rawat83@gmail.com>
2022-09-07 19:29:49 +0800

    contract structure added

--- contracts/DigitalVault.sol
+++ contracts/DigitalVault.sol
@@ -26,8 +26,11 @@ contract DigitalVault is Ownable, Pausable {
   mapping(address => address) public nomineeUserDetails;
 
   error NullAddress();
-  error InvalidShare(string, uint);
+  error ZeroAmountError(uint txAmount);
+  error InvalidShare(string message, uint share);
 
+  event fundDeposited(address indexed depositor, uint amount, uint DepositBlockNumber);
+  event fundWithdraw(address indexed withdrawer, uint amount, uint WithdrawalBlockNumber);
   event nomineeAdded(address indexed _n);
   event nomineeRemoved(address indexed _n);
   event Log(address sender, uint value);
@@ -36,11 +39,11 @@ contract DigitalVault is Ownable, Pausable {
     _expectedBlocksBetweenTwoSignatures = _signatureDuration;
   }
 
- /*
- * Add nominee for the user
- * @param  {address} _n address of nominee
- * @param  {uint} _s % share for the nominee
- */
+  /*
+  * Add nominee for the user
+  * @param  {address} _n address of nominee
+  * @param  {uint} _s % share for the nominee
+  */
   function addNominee(address _n, uint _s) public {
     if (_n == address(0))       // non Null Check
       revert NullAddress();
@@ -64,27 +67,67 @@ contract DigitalVault is Ownable, Pausable {
     // OR
     // userNomineeData[msg.sender].push(NomineeDetails(_n, _s));
   }
- 
-/*
- * Add nominee for the user
- * @return {tuple(address, uint)} get the nominee details for the user
- */
-  function deposit() external {
 
+  /*
+  * remove nominee for the user
+  * @param  {address} _n address of nominee
+  */
+  function removeNominee(address _n) public { 
+    // add logic
   }
 
-/*
- * User or Nominee can withdraw their allocated funds
- * @return {tuple(address, uint)} get the nominee details for the user
- */
+  /*
+  * User should able to deposit funds into Vault
+  */
+  function deposit() external payable {
+    if(msg.value <= 0)
+      revert ZeroAmountError(msg.value);
+
+  // Update user Balance 
+    userBalance[tx.origin] = msg.value;
+    emit fundDeposited(tx.origin, msg.value, block.number);
+  }
+
+  /*
+  * User or Nominee can withdraw their allocated funds
+  */
   function withdraw() external whenNotPaused {
+    // add logic
+    // Using this function users and nominee should be able to withdraw their funds
+    // Function should check if the user has sufficient balance or not
+    // Function should check whether isUserFundReadyForInheritance is True or not
+    // Function should check whether the sender is in nomineeUserDetails mapping or not
+    // Function should Update the OwnerBalance mapping
+    // Function should emit event FundWithdrawal(sender, amount)
+    // Get the user details using nomineeUserDetails[msg.sender] and then check the %share he owns userNomineeData[user][msg.sender]
 
   }
 
-/*
- * Emergency Control - Pause
- * @return {tuple(address, uint)} get the nominee details for the user
- */
+  /*
+  * Validate the user liveness
+  * @param  {address} _n address of user
+  * @return {bool} true / false based on the user's sig
+  */
+  function isAlive(address user) public returns(bool) {
+    // add logic
+    // Current Block - LastSignedBlock > expectedBlocksBetweenTwoSignatures   Return False 
+    // Else  Current Block - LastSignedBlock < expectedBlocksBetweenTwoSignatures   Return True 
+  } 
+
+  /*
+  * Validate if user funds are ready for Inheritance
+  * @param  {address} _n address of user
+  * @return {bool} true / false based on the user's sig
+  */
+  function isUserFundReadyForInheritance(address user) public returns (bool) {
+    // add logic
+    // Current Block - LastSignedBlock < 2 x expectedBlocksBetweenTwoSignatures   Return False Else  
+    // Current Block - LastSignedBlock > 2 x expectedBlocksBetweenTwoSignatures   Return True
+  }
+
+  /*
+  * Emergency Control - Pause
+  */
   function pause() external onlyOwner {
     _pause();
   }
